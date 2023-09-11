ffb435081f9507fdad9c6cd6b2ac0faa86a8c5de
inaz2 <inaz2@users.noreply.github.com>
2023-09-11 20:58:26 +0900

    Remove (*GoSplit).openFileOrStdin

--- gosplit/gosplit.go
+++ gosplit/gosplit.go
@@ -25,6 +25,107 @@ func New(filePath string, prefix string) *GoSplit {
 	}
 }
 
+// SetOutDir changes the directory of output files.
+//
+// This method is mainly for testing.
+func (g *GoSplit) SetOutDir(outDir string) {
+	g.outDir = outDir
+}
+
+// ByLines splits the content of filePath by nLines.
+func (g *GoSplit) ByLines(nLines int) error {
+	if g.prefix == "" {
+		return fmt.Errorf("prefix must not be empty string")
+	}
+	if nLines <= 0 {
+		return fmt.Errorf("nLines must be larger than zero")
+	}
+
+	var rFile *os.File
+	if g.filePath == "-" {
+		rFile = os.Stdin
+	} else {
+		f, err := os.Open(g.filePath)
+		if err != nil {
+			return fmt.Errorf("failed to open: %w", err)
+		}
+		defer f.Close()
+		rFile = f
+	}
+
+	if err := g.byLinesInternal(rFile, nLines); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ByNumber splits the content of filePath into nNumber files.
+func (g *GoSplit) ByNumber(nNumber int) error {
+	if g.prefix == "" {
+		return fmt.Errorf("prefix must not be empty string")
+	}
+	if nNumber <= 0 {
+		return fmt.Errorf("nNumber must be larger than zero")
+	}
+
+	var rFile *os.File
+	if g.filePath == "-" {
+		// print error message when filePath is stdin
+		return fmt.Errorf("cannot determine file size")
+	} else {
+		f, err := os.Open(g.filePath)
+		if err != nil {
+			return fmt.Errorf("failed to open: %w", err)
+		}
+		defer f.Close()
+		rFile = f
+	}
+
+	fileInfo, err := rFile.Stat()
+	if err != nil {
+		return fmt.Errorf("failed to stat: %w", err)
+	}
+	fileSize := fileInfo.Size()
+	if fileSize == 0 {
+		return nil
+	}
+
+	if err := g.byNumberInternal(rFile, fileSize, nNumber); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ByBytes splits the content of filePath by nBytes.
+func (g *GoSplit) ByBytes(nBytes int64) error {
+	if g.prefix == "" {
+		return fmt.Errorf("prefix must not be empty string")
+	}
+	if nBytes <= 0 {
+		return fmt.Errorf("nBytes must be larger than zero")
+	}
+
+	var rFile *os.File
+	if g.filePath == "-" {
+		rFile = os.Stdin
+	} else {
+		f, err := os.Open(g.filePath)
+		if err != nil {
+			return fmt.Errorf("failed to open: %w", err)
+		}
+		defer f.Close()
+		rFile = f
+	}
+
+	if err := g.byBytesInternal(rFile, nBytes); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // generateOutFilePath returns n-th output file name with prefix.
 //
 // only support 2-character suffix; aa , ab, ..., zz.
@@ -44,39 +145,9 @@ func (g *GoSplit) generateOutFilePath(number int) (string, error) {
 	return outFilePath, nil
 }
 
-// openFileOrStdin opens filePath or returns os.Stdin.
-func (g *GoSplit) openFileOrStdin() (*os.File, error) {
-	if g.filePath == "-" {
-		return os.Stdin, nil
-	} else {
-		rFile, err := os.Open(g.filePath)
-		return rFile, err
-	}
-}
-
-// SetOutDir changes the directory of output files.
-//
-// This method is mainly for testing.
-func (g *GoSplit) SetOutDir(outDir string) {
-	g.outDir = outDir
-}
-
-// ByLines splits the content of filePath by nLines.
-func (g *GoSplit) ByLines(nLines int) error {
-	if g.prefix == "" {
-		return fmt.Errorf("prefix must not be empty string")
-	}
-	if nLines <= 0 {
-		return fmt.Errorf("nLines must be larger than zero")
-	}
-
-	rFile, err := g.openFileOrStdin()
-	if err != nil {
-		return fmt.Errorf("failed to open: %w", err)
-	}
-	defer rFile.Close()
-
-	scanner := bufio.NewScanner(rFile)
+// byLinesInternal splits the content from io.Reader by nLines.
+func (g *GoSplit) byLinesInternal(r io.Reader, nLines int) error {
+	scanner := bufio.NewScanner(r)
 
 OuterLoop:
 	for i := 0; ; i++ {
@@ -106,34 +177,8 @@ OuterLoop:
 	return nil
 }
 
-// ByNumber splits the content of filePath into nNumber files.
-func (g *GoSplit) ByNumber(nNumber int) error {
-	// print error message when filePath is stdin
-	if g.filePath == "-" {
-		return fmt.Errorf("cannot determine file size")
-	}
-
-	if g.prefix == "" {
-		return fmt.Errorf("prefix must not be empty string")
-	}
-	if nNumber <= 0 {
-		return fmt.Errorf("nNumber must be larger than zero")
-	}
-
-	rFile, err := os.Open(g.filePath)
-	if err != nil {
-		return fmt.Errorf("failed to open: %w", err)
-	}
-	defer rFile.Close()
-
-	fileInfo, err := rFile.Stat()
-	if err != nil {
-		return fmt.Errorf("failed to stat: %w", err)
-	}
-	fileSize := fileInfo.Size()
-	if fileSize == 0 {
-		return nil
-	}
+// byNumberInternal splits the content from io.Reader into nNumber files.
+func (g *GoSplit) byNumberInternal(r io.Reader, fileSize int64, nNumber int) error {
 	chunkSize := fileSize / int64(nNumber)
 
 	for i := 0; i < nNumber; i++ {
@@ -147,7 +192,7 @@ func (g *GoSplit) ByNumber(nNumber int) error {
 		}
 		// the last file size should be larger than or equal to chunkSize
 		if i < nNumber-1 {
-			written, err := io.CopyN(wFile, rFile, chunkSize)
+			written, err := io.CopyN(wFile, r, chunkSize)
 			if written < chunkSize {
 				if written == 0 {
 					defer os.Remove(outFilePath)
@@ -158,7 +203,7 @@ func (g *GoSplit) ByNumber(nNumber int) error {
 				return fmt.Errorf("failed to write: %w", err)
 			}
 		} else {
-			if _, err := io.Copy(wFile, rFile); err != nil {
+			if _, err := io.Copy(wFile, r); err != nil {
 				return fmt.Errorf("failed to write: %w", err)
 			}
 		}
@@ -167,21 +212,8 @@ func (g *GoSplit) ByNumber(nNumber int) error {
 	return nil
 }
 
-// ByBytes splits the content of filePath by nBytes.
-func (g *GoSplit) ByBytes(nBytes int64) error {
-	if g.prefix == "" {
-		return fmt.Errorf("prefix must not be empty string")
-	}
-	if nBytes <= 0 {
-		return fmt.Errorf("nBytes must be larger than zero")
-	}
-
-	rFile, err := g.openFileOrStdin()
-	if err != nil {
-		return fmt.Errorf("failed to open: %w", err)
-	}
-	defer rFile.Close()
-
+// byBytesInternal splits the content from io.Reader by nBytes.
+func (g *GoSplit) byBytesInternal(r io.Reader, nBytes int64) error {
 	for i := 0; ; i++ {
 		outFilePath, err := g.generateOutFilePath(i)
 		if err != nil {
@@ -191,7 +223,7 @@ func (g *GoSplit) ByBytes(nBytes int64) error {
 		if err != nil {
 			return fmt.Errorf("failed to create: %w", err)
 		}
-		written, err := io.CopyN(wFile, rFile, nBytes)
+		written, err := io.CopyN(wFile, r, nBytes)
 		if written < nBytes {
 			if written == 0 {
 				defer os.Remove(outFilePath)
