f2a17e1647742257d43e66798a8ae5d3cd348735
inaz2 <inaz2@users.noreply.github.com>
2023-09-18 16:16:32 +0900

    Update safeint.go

--- gosplit/safeint.go
+++ gosplit/safeint.go
@@ -1,40 +1,57 @@
 package gosplit
 
 // safeMulInt64 return x*y with checking integer overflow
-func safeMulInt64(x int64, y int64) (int64, bool) {
+func safeMulInt64(x int64, y int64) (int64, error) {
 	z := x * y
 	if y != 0 && z/y != x {
-		// integer overflow occured
-		return 0, false
+		return 0, GoSplitErrorf("integer overflow occured: %#v * %#v -> %#v", x, y, z)
 	}
-	return z, true
+	return z, nil
 }
 
 // safePowInt64 returns b**k with checking integer overflow
-func safePowInt64(b int64, k int64) (int64, bool) {
-	var ok bool
+func safePowInt64(b int64, k int64) (int64, error) {
+	var err error
 
-	if k < 0 {
-		return 0, true
+	if b == 1 {
+		return 1, nil
+	}
+	if b == 0 {
+		if k == 0 {
+			return 1, nil
+		} else if k > 0 {
+			return 0, nil
+		} else {
+			return 0, GoSplitErrorf("division by zero: 0 ** (%#v)", k)
+		}
+	}
+	if b == -1 {
+		if k == 0 {
+			return 1, nil
+		} else if k > 0 {
+			return (-1) * k, nil
+		} else {
+			return (-1) * (-k), nil
+		}
 	}
 
 	result := int64(1)
 	x := b
 	for {
 		if k&1 == 1 {
-			result, ok = safeMulInt64(result, x)
-			if !ok {
-				return 0, false
+			result, err = safeMulInt64(result, x)
+			if err != nil {
+				return 0, err
 			}
 		}
 		k >>= 1
 		if k <= 0 {
 			break
 		}
-		x, ok = safeMulInt64(x, x)
-		if !ok {
-			return 0, false
+		x, err = safeMulInt64(x, x)
+		if err != nil {
+			return 0, err
 		}
 	}
-	return result, true
+	return result, nil
 }
